eb70202bbce345bdd1b69b9505ccc876dd2ff961
Sullivan008 <kozakpeter0424@gmail.com>
2020-04-21 11:10:53 +0200

    - Container includes Top and Bottom Padding (Create required CSS in app.components.css); - Created a Footer Component and then implemented the required Component in app.component.html.; - Implemented the Shopping List Template with static data in shopping-list.component.html (This is what it will look like with release data).; - Implemented a Shopping Edit Template in shopping-edit-component.html (This is what it will look like with release data)

--- src/app/shopping-list/shopping-edit/shopping-edit.component.ts
+++ src/app/shopping-list/shopping-edit/shopping-edit.component.ts
@@ -1,11 +1,16 @@
 import { Component, OnInit } from '@angular/core';
 
+import { faPlus, faTrashAlt, faSyncAlt, IconDefinition } from '@fortawesome/free-solid-svg-icons';
+
 @Component({
   selector: 'app-shopping-edit',
   templateUrl: './shopping-edit.component.html',
   styleUrls: ['./shopping-edit.component.css']
 })
 export class ShoppingEditComponent implements OnInit {
+  plusIcon: IconDefinition = faPlus;
+  trashIcon: IconDefinition = faTrashAlt;
+  clearIcon: IconDefinition = faSyncAlt;
 
   constructor() { }
 
