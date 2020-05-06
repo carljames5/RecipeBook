83e90a3163d15d9b0c9e664e7016b90f1f90ed07
Sullivan008 <kozakpeter0424@gmail.com>
2020-05-06 19:27:38 +0200

    Removed onNavigate Event from header.component.ts file; Removed href links and click events from a HTML Tags, and add routerLink attribute for a HTML Tags.

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,4 +1,4 @@
-import { Component, EventEmitter, Output } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
     selector: 'app-header',
@@ -6,9 +6,5 @@ import { Component, EventEmitter, Output } from '@angular/core';
     styleUrls: ['./header.component.css']
 })
 export class HeaderComponent {
-    @Output() navigation = new EventEmitter<string>();
 
-    onNavigate(selectedMenu: string) {
-        this.navigation.emit(selectedMenu);
-    }
 }
