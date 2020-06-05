3a9d98b4fc9d0e5a11cb1fb1a5349ff3005ab62a
Sullivan008 <kozakpeter0424@gmail.com>
2020-06-05 10:05:00 +0200

    Refactor   - Delete the getIngredientIndexByIdAndName method and use only getIngredientIndexById instead.   - Run only update method if the index value is not -1.   - We use existing methods for searches.

--- src/app/shopping-list/shopping-list.service.ts
+++ src/app/shopping-list/shopping-list.service.ts
@@ -11,15 +11,11 @@ export class ShoppingListService {
     public ingredientsChanged = new Subject<Ingredient[]>();
     public ingredientEditing = new Subject<number>();
 
-    public getIngredients(): Ingredient[] {
-        return this.ingredients.slice();
-    }
-
     public addIngredient(ingredient: Ingredient): void {
         const ingredientIndex = this.getIngredientIndexByName(ingredient);
 
         if(ingredientIndex !== -1) {
-            let modifiedIngredient = this.ingredients[ingredientIndex];
+            let modifiedIngredient = this.getIngredientById(ingredient.id);
             modifiedIngredient.amount += ingredient.amount;
 
             this.ingredients[ingredientIndex] = modifiedIngredient;
@@ -34,10 +30,10 @@ export class ShoppingListService {
 
     public addIngredients(ingredients: Ingredient[]): void {
         ingredients.forEach(item => {
-            const ingredientIndex = this.getIngredientIndexByIdAndName(item);
+            const ingredientIndex = this.getIngredientIndexById(item);
 
             if(ingredientIndex !== -1) {
-                let updatedIngredient = this.ingredients.find(x => x.id == item.id);
+                let updatedIngredient = this.getIngredientById(item.id);
                 updatedIngredient.amount += item.amount;
                 
                 this.ingredients[ingredientIndex] = updatedIngredient;
@@ -49,31 +45,36 @@ export class ShoppingListService {
         this.ingredientsChanged.next(this.ingredients.slice());
     }
 
+    public updateIngredient(updatedIngredient: Ingredient): void {
+        const updatedIngredientIndex = this.getIngredientIndexById(updatedIngredient);
+        
+        if (updatedIngredientIndex !== -1) {
+            this.ingredients[updatedIngredientIndex] = updatedIngredient;
+            
+            this.ingredientsChanged.next(this.ingredients.slice());
+        }
+    }
+    
+    public deleteIngredient(id: number): void {
+        const deleteIngredientIndex = this.getIngredientIndexById(this.getIngredientById(id));
+        
+        if (deleteIngredientIndex !== -1) {
+            this.ingredients.splice(deleteIngredientIndex, 1);
+            
+            this.ingredientsChanged.next(this.ingredients.slice());
+        }
+    }
+    
+    public getIngredients(): Ingredient[] {
+        return this.ingredients.slice();
+    }
+
     public getIngredientById(id: number) : Ingredient {
         return this.ingredients.find(x => x.id == id);
     }
 
-    public updateIngredient(updatedIngredient: Ingredient): void {
-        const updatedIngredientIndex = this.ingredients.findIndex(x => x.id == updatedIngredient.id);
-
-        this.ingredients[updatedIngredientIndex] = updatedIngredient;
-
-        this.ingredientsChanged.next(this.ingredients.slice());
-    }
-
-    public deleteIngredient(id: number): void {
-        const deleteIngredientIndex = this.getIngredientIndexByIdAndName(this.getIngredientById(id));
-
-        if(deleteIngredientIndex !== -1) {
-            this.ingredients.splice(deleteIngredientIndex, 1);
-    
-            this.ingredientsChanged.next(this.ingredients.slice());
-        }
-    }
-
-    private getIngredientIndexByIdAndName(ingredient: Ingredient) {
-        return this.ingredients
-                   .findIndex(x => x.id == ingredient.id && x.name.toLowerCase().trim() === ingredient.name.toLowerCase().trim())
+    private getIngredientIndexById(ingredient: Ingredient) {
+        return this.ingredients.findIndex(x => x.id === ingredient.id);
     }
 
     private getIngredientIndexByName(ingredient: Ingredient) {
