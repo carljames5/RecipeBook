0e3a3bc15d6c6dc484dcc72fbdf856520ddac6af
Sullivan008 <kozakpeter0424@gmail.com>
2020-10-03 14:46:54 +0200

    * When the user signed in the application then the signIn service navigate to root path.

--- Angular-RecipeBook-FrontEnd/src/app/modules/authentication/services/authentication.service.ts
+++ Angular-RecipeBook-FrontEnd/src/app/modules/authentication/services/authentication.service.ts
@@ -1,11 +1,12 @@
+import { Router } from '@angular/router';
+import { finalize } from 'rxjs/operators';
 import { Injectable } from '@angular/core';
 
 import { AuthenticationHttpService } from './authentication-http.service';
 import { LoadingSpinnerService } from 'src/app/core/services/loading-spinner.service';
 
-import { SignInRequestModel } from '../models/request-models/sign-in-request.model';
-import { finalize } from 'rxjs/operators';
 import { SignOutModel } from '../models/sign-out.model';
+import { SignInRequestModel } from '../models/request-models/sign-in-request.model';
 
 @Injectable({
   providedIn: 'root',
@@ -13,7 +14,8 @@ import { SignOutModel } from '../models/sign-out.model';
 export class AuthenticationService {
   public constructor(
     private authenticationHttpService: AuthenticationHttpService,
-    private loadingSpinnerService: LoadingSpinnerService
+    private loadingSpinnerService: LoadingSpinnerService,
+    private router: Router
   ) {}
 
   public signIn(signInFormValue: any) {
@@ -28,7 +30,9 @@ export class AuthenticationService {
     this.authenticationHttpService
       .signIn(requestModel)
       .pipe(finalize(() => this.loadingSpinnerService.hide()))
-      .subscribe(() => {});
+      .subscribe(() => {
+        this.router.navigate(['/']);
+      });
   }
 
   public signOut(signOutModel: SignOutModel) {
