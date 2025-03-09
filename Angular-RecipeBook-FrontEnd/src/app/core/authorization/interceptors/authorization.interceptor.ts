488cd321fdd0fac0ca98a9f475ca3332e254c9dd
carljames5 <carl.james5025@gmail.com>
2025-03-10 01:37:56 +0900

    fix

--- Angular-RecipeBook-FrontEnd/src/app/core/authorization/interceptors/authorization.interceptor.ts
+++ Angular-RecipeBook-FrontEnd/src/app/core/authorization/interceptors/authorization.interceptor.ts
@@ -1,18 +1,12 @@
 import { Observable } from 'rxjs';
-import { Injectable } from '@angular/core';
 import { HttpEvent, HttpHandler, HttpInterceptor, HttpRequest } from '@angular/common/http';
 
-@Injectable({
-  providedIn: 'root',
-})
 export class AuthorizationInterceptor implements HttpInterceptor {
   intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
-    const clonedRequest = req.clone({
+    req = req.clone({
       withCredentials: true,
-      // Optionally add authorization headers here
-      // headers: req.headers.set('Authorization', `Bearer ${yourToken}`)
     });
 
-    return next.handle(clonedRequest);
+    return next.handle(req);
   }
 }
